9e319d885ff23f78c2f4e0a1d05bd38ad44fb73a
Ruozhen(Gabriel) GU <gu.gabriel@hotmail.com>
2018-04-13 21:17:42 -0400

    Add files via upload

---
+++ bishop.h
@@ -0,0 +1,27 @@
+//
+// Created by Gabriel Gu on 2018-03-27.
+//
+
+#ifndef A5_BISHOP_H
+#define A5_BISHOP_H
+
+#include "piece.h"
+#include <iostream>
+#include <string>
+class Board;
+
+class Bishop : public Piece {
+
+public:
+    Bishop(int x, int y, bool alive, char name, Board *b);
+    void produce_next()override;
+    bool isblock(int r, int c)override;
+    bool attack(int x, int y)override;
+
+};
+
+
+#endif //A5_BISHOP_H
+
+
+
---
+++ piece.cc
@@ -0,0 +1,24 @@
+//
+// Created by Gabriel Gu on 2018-03-27.
+//
+
+#include "piece.h"
+#include <iostream>
+using namespace std;
+
+bool Piece::attack(int x, int y) {
+	for (auto it = next_move.begin(); it != next_move.end(); it++) {
+		if ((*it)[0] == x && (*it)[1] == y) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Piece::is_alive() { return alive; }
+
+char Piece::get_name() { return chess_name; }
+
+
+
+
